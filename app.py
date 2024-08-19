861dec74fb686eca799261e16c041e4b3eb9d470
Alannikos <156394688+Alannikos@users.noreply.github.com>
2024-08-19 21:24:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ os.system(f'cd ./LLM/model/ && git lfs pull')
 
 # TTS model
 os.system(f'git lfs install')
-os.system(f'git clone https://huggingface.co/2Noise/ChatTTS ./TTS/weights/ChatTTS')
+os.system(f'git clone https://hf-mirror.com/2Noise/ChatTTS ./TTS/weights/ChatTTS')
 
 # ASR model
 os.system(f'git lfs install')
-os.system(f'git clone git@hf.co:FunAudioLLM/SenseVoiceSmall ./ASR/SenseVoiceSmall/')
+os.system(f'git clone https://hf-mirror.com/FunAudioLLM/SenseVoiceSmall ./ASR/SenseVoiceSmall/')
 
 
 
